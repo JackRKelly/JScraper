d1b281e169cfc136310a5abd881440f75f28a0a9
MyManVan <kcjackkelly@gmail.com>
2020-05-13 12:33:47 -0500

    Added validate URL method

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,32 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
+    private static boolean validateUrl(String url) {
+        String urlRegex = "^(http|https)://[-a-zA-Z0-9+&@#/%?=~_|,!:.;]*[-a-zA-Z0-9+@#/%=&_|]";
+        Pattern pattern = Pattern.compile(urlRegex);
+        Matcher m = pattern.matcher(url);
+        if (m.matches()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public static void scrapeLinks(String link) {
         System.out.println(link);
         try {
-              
+
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        scrapeLinks(args[0]);
+        if (!validateUrl(args[0])) {
+            System.out.println("Please enter a valid link.");
+        } else {
+            scrapeLinks(args[0]);
+        }
     }
 }
