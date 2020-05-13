3ffdf36a424ee37546beca0d16bbc52d12e31926
MyManVan <kcjackkelly@gmail.com>
2020-05-13 11:11:29 -0500

    Added main function which scrapes user argument

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void scrapeLinks(String link) {
+        System.out.println(link);
+    }
+
+    public static void main(String[] args) {
+        scrapeLinks(args[0]);
+    }
 }
