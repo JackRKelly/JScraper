12585ea647206cffa6c67600c9ea2c786640d1a5
MyManVan <kcjackkelly@gmail.com>
2020-05-13 11:13:19 -0500

    Added error handling to try catch method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void scrapeLinks(String link) {
         System.out.println(link);
+        try {
+              
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
