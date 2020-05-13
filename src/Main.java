cad549e27b13408be2ded745adf434d84160712f
MyManVan <kcjackkelly@gmail.com>
2020-05-13 13:56:37 -0500

    Added print urls method

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
+    private static boolean printUrls(String[] urls) {
+
+    }
+
     private static boolean validateUrl(String url) {
         String urlRegex = "^(http|https)://[-a-zA-Z0-9+&@#/%?=~_|,!:.;]*[-a-zA-Z0-9+@#/%=&_|]";
         Pattern pattern = Pattern.compile(urlRegex);
@@ -21,10 +25,11 @@ public class Main {
     public static void scrapeLinks(String url) {
         String urlRegex = "<a\\s+(?:[^>]*?\\s+)?href=([\"'])(.*?)\\1";
         Pattern pattern = Pattern.compile(urlRegex);
-        String[] matchs;
+        String[] final_matches;
         try {
             final Document document = Jsoup.connect(url).get();
             Matches matches = pattern.matches(document);
+            printUrls(matches);
         } catch (IOException e) {
             e.printStackTrace();
         }
